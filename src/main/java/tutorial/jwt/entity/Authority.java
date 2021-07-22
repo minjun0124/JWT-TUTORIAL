7c7e62b72ca698a5e267c6062f20555a9d2fe739
mjlee <minjun0124@gmail.com>
2021-07-22 15:39:57 +0900

    feat: SpringSecurity config with jwt

---
+++ src/main/java/tutorial/jwt/entity/Authority.java
@@ -0,0 +1,26 @@
+package tutorial.jwt.entity;
+
+import lombok.*;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "authority")
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Authority {
+
+    /**
+     * 권한명
+     */
+    @Id
+    @Column(name = "authority_name", length = 50)
+    private String authorityName;
+
+}
